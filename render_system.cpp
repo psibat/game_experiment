b041bfe934f288ecf0bb934f42fdb64daa306ca2
psibat <psibat@hotmail.co.uk>
2018-05-26 14:44:56 +0100

    Added log to render

--- render_system.cpp
+++ render_system.cpp
@@ -1,5 +1,9 @@
-#include <ncurses.h>
 #include "render_system.h"
+
+#include <string>
+#include <ncurses.h>
+
+#include "logger.h"
  
 RenderSystem::RenderSystem(EntityManager &entity_manager) : System(entity_manager){ }
 
@@ -9,6 +13,8 @@ void RenderSystem::work() {
 			Position *position = entity_manager.position(i);
 			Appearance *appearance = entity_manager.appearance(i);
 			mvaddch(position->y, position->x, appearance->model);
+
+			log("Drawing entity at: " + std::to_string(position->y) + " " + std::to_string( position->x));
 		}
 	}
 }
